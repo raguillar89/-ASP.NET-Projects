f4bd632feb39cb43c05050a4f9ca3a0a9394a465
Renan Pinho <renan_aguillarpinho@hotmail.com>
2022-10-26 11:40:18 -0300

    Criação o SellerService e FindAll Sellers

--- SalesWebMvc/SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/SalesWebMvc/Data/SeedingService.cs
@@ -29,11 +29,11 @@ namespace SalesWebMvc.Data
             Department d4 = new Department(4, "Softwares");
 
             Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
-            Seller s2 = new Seller(2, "Maria Green", "maria@gmail.com", new DateTime(1979, 12, 31), 1000.0, d2);
-            Seller s3 = new Seller(3, "Alex Grey", "alex@gmail.com", new DateTime(1998, 1, 15), 1000.0, d1);
-            Seller s4 = new Seller(4, "Martha Red", "martha@gmail.com", new DateTime(1998, 11, 30), 1000.0, d4);
-            Seller s5 = new Seller(5, "Donald Blue", "donald@gmail.com", new DateTime(1998, 1, 9), 1000.0, d3);
-            Seller s6 = new Seller(6, "Arnold Pink", "arnold@gmail.com", new DateTime(1998, 3, 4), 1000.0, d2);
+            Seller s2 = new Seller(2, "Maria Green", "maria@gmail.com", new DateTime(1979, 12, 31), 2000.0, d2);
+            Seller s3 = new Seller(3, "Alex Grey", "alex@gmail.com", new DateTime(1999, 1, 15), 3000.0, d1);
+            Seller s4 = new Seller(4, "Martha Red", "martha@gmail.com", new DateTime(1983, 11, 30), 4500.0, d4);
+            Seller s5 = new Seller(5, "Donald Blue", "donald@gmail.com", new DateTime(1987, 1, 9), 2500.0, d3);
+            Seller s6 = new Seller(6, "Arnold Pink", "arnold@gmail.com", new DateTime(1996, 3, 4), 3400.0, d2);
 
             SalesRecord r1 = new SalesRecord(1, new DateTime(2018, 09, 25), 11000.0, SaleStatus.Billed, s1);
             SalesRecord r2 = new SalesRecord(2, new DateTime(2018, 09, 4), 7000.0, SaleStatus.Billed, s5);
